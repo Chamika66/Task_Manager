bda81219fde2973f4541a63db8a49949eecc4b39
Chamika66 <121974626+Chamika66@users.noreply.github.com>
2024-09-11 10:42:12 +0530

    Initial commit

---
+++ src/main/java/DTO/Task.java
@@ -0,0 +1,59 @@
+package DTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String description;
+    private String status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/test/java/org/codeschool/springfirst/SpringFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeschool.springfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
